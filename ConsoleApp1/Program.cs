db58482516cda3e2c55e5c7be1c1a4a3f8b8757f
mfurqan <mfurqan@innovative-pk.com>
2023-05-20 14:52:40 +0500

    Write a method to show serial number

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,41 +14,47 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            string imagePath = "C:\\Users\\mfurqan\\Desktop\\Fetch Image\\1.jpg";
+           string imagePath = "C:\\Users\\mfurqan\\Desktop\\Fetch Image\\1.jpg";
+            getSerial_no(imagePath);
+        }
+
+        static void getSerial_no(string imagepath)
+        {
+            string imagePath = imagepath;
             string tessdataPath = @"C:\Users\mfurqan\source\repos\OCR_ConsoleApp\packages\Tesseract.5.2.0\tessdata";
-           
+
             // Specify the Tesseract engine options
             var engineOptions = new TesseractEngineConfig
             {
                 TessDataPrefix = tessdataPath
             };
 
-                try
+            try
             {
                 // Create a Tesseract instance
                 using (TesseractEngine engine = new TesseractEngine(tessdataPath, "eng", EngineMode.Default))
-            {
-                // Load the image
-                using (var image = Pix.LoadFromFile(imagePath))
                 {
-                    // Apply OCR on the image
-                    using (var page = engine.Process(image))
+                    // Load the image
+                    using (var image = Pix.LoadFromFile(imagePath))
                     {
-                        // Retrieve the recognized text
-                        string extractedText = page.GetText();
+                        // Apply OCR on the image
+                        using (var page = engine.Process(image))
+                        {
+                            // Retrieve the recognized text
+                            string extractedText = page.GetText();
 
-                        // Print the extracted text
-                        Console.WriteLine(extractedText);
+                            // Print the extracted text
+                            Console.WriteLine(extractedText);
+                        }
                     }
                 }
-            }
 
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                Console.WriteLine("Message: "+ ex);
+                Console.WriteLine("Message: " + ex);
             }
-            
+
             Console.ReadLine();
         }
     }
